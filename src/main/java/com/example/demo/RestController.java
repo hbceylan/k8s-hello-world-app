a8be0f79e792f9fb1bb85e533c4732ef7ee3813b
Halid Busayr Ceylan <halid@cimri.com>
2018-09-25 16:30:31 +0300

    added actuator

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
